a53e3fef55fb44826000fcac322e104339eb0734
Suraj <skp.my12@gmail.com>
2023-05-11 19:28:38 +0530

    changes

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const express = require('express')
 connectToMongo();
 const app = express()
 const port = 5000
+var cors = require('cors')
+
 
 //available routes
 // app.get('/', (req, res) => {
 //   res.send('Hello suraj')
 // })
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', require ('./routes/auth'))
